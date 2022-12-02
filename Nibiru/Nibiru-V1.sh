39b97e6b25a02425de668c083ab2ae1b08ebdf7a
Kripto Kurdu <119195768+KriptoKurdu@users.noreply.github.com>
2022-12-02 19:54:48 +0300

    Update Nibiru-V1.sh

--- Nibiru/Nibiru-V1.sh
+++ Nibiru/Nibiru-V1.sh
@@ -62,24 +62,34 @@ cp genesis.json $HOME/.nibid/config/genesis.json
 
 # Pers ayarlama
 
-PEERS="751f76832ae3aefe9373ee697f4699c4bd0acafc@161.97.136.141:26656,93137cb574b5d6bd6fdb60e6c8164a08c1516081@209.126.8.192:26656,968472e8769e0470fadad79febe51637dd208445@65.108.6.45:60656,573946fee0377eb9448bfb7cb731ffbb41b2b078@38.242.214.172:2486,2e90413e61ff3b7f1098c7cd46a283142af133ce@80.65.211.115:26656,ff597c3eea5fe832825586cce4ed00cb7798d4b5@65.109.53.53:26656,145c04540ea9fad36d97a6c37b66d134e19a5450@38.242.152.235:26656,51625ddeba19faec101fe10423315856f82257ca@51.222.155.224:26656,2b93c4402a26adc73e043d9a35f3cedd4aea311b@149.102.129.200:26656,eb65c95ea745d1cb5f66e2fda5d5e1029f4dc43d@5.161.43.109:26656,f30138f9d0c986b511ea476eb199fead17a62ae4@45.67.217.120:46656,5d46e8a12044703e3a711462261652b0164a3ca6@194.233.84.166:39656,ae357e14309640ca33cde597b37f0a91e63a32bd@144.76.90.130:36656,358a862e6851d8afe4efb7e0a9223b770a745eb0@34.142.140.178:39656,7445531c80f47f5469d2244d59276fa4f569515e@161.97.105.100:26656,75df3cedd70ef5db343278eb67e94a41949358b1@45.67.217.225:46656,88cfcca95e534bf32092a8d70cff646cf46d8202@154.26.132.182:26656,d8b4d4ddef3e99d2a66dbc25568ce6e91c7d5568@43.156.112.23:26656,6794490764f688fde88befee0340eaea022cd8d1@161.97.105.44:26656,659030eeffba5cf38c7d5f66bd46447d2048d443@62.171.171.178:26656,3997242f9646ca642932852b7577ddb9976e0396@5.199.130.53:26656,213d1398817da527b4c3c05f4fe64302382d6733@146.190.99.66:39656"
+PEERS="93137cb574b5d6bd6fdb60e6c8164a08c1516081@209.126.8.192:26656,968472e8769e0470fadad79febe51637dd208445@65.108.6.45:60656,573946fee0377eb9448bfb7cb731ffbb41b2b078@38.242.214.172:2486,2e90413e61ff3b7f1098c7cd46a283142af133ce@80.65.211.115:26656,ff597c3eea5fe832825586cce4ed00cb7798d4b5@65.109.53.53:26656,145c04540ea9fad36d97a6c37b66d134e19a5450@38.242.152.235:26656,51625ddeba19faec101fe10423315856f82257ca@51.222.155.224:26656,2b93c4402a26adc73e043d9a35f3cedd4aea311b@149.102.129.200:26656,eb65c95ea745d1cb5f66e2fda5d5e1029f4dc43d@5.161.43.109:26656,f30138f9d0c986b511ea476eb199fead17a62ae4@45.67.217.120:46656,5d46e8a12044703e3a711462261652b0164a3ca6@194.233.84.166:39656,ae357e14309640ca33cde597b37f0a91e63a32bd@144.76.90.130:36656,358a862e6851d8afe4efb7e0a9223b770a745eb0@34.142.140.178:39656,7445531c80f47f5469d2244d59276fa4f569515e@161.97.105.100:26656,75df3cedd70ef5db343278eb67e94a41949358b1@45.67.217.225:46656,88cfcca95e534bf32092a8d70cff646cf46d8202@154.26.132.182:26656,d8b4d4ddef3e99d2a66dbc25568ce6e91c7d5568@43.156.112.23:26656,6794490764f688fde88befee0340eaea022cd8d1@161.97.105.44:26656,659030eeffba5cf38c7d5f66bd46447d2048d443@62.171.171.178:26656,3997242f9646ca642932852b7577ddb9976e0396@5.199.130.53:26656"
 sed -i.bak -e "s/^persistent_peers *=.*/persistent_peers = \"$PEERS\"/" $HOME/.nibid/config/config.toml
 
-
+sed -i 's/minimum-gas-prices =.*/minimum-gas-prices = "0.025unibi"/g' $HOME/.nibid/config/app.toml
 
 #Blok süresi ayarlama
-
- sed -i 's/timeout_propose =.*/timeout_propose = "100ms"/g' $HOME/.nibid/config/config.toml
- sed -i 's/timeout_propose_delta =.*/timeout_propose_delta = "500ms"/g' $HOME/.nibid/config/config.toml
- sed -i 's/timeout_prevote =.*/timeout_prevote = "100ms"/g' $HOME/.nibid/config/config.toml
- sed -i 's/timeout_prevote_delta =.*/timeout_prevote_delta = "500ms"/g' $HOME/.nibid/config/config.toml
- sed -i 's/timeout_precommit =.*/timeout_precommit = "100ms"/g' $HOME/.nibid/config/config.toml
- sed -i 's/timeout_precommit_delta =.*/timeout_precommit_delta = "500ms"/g' $HOME/.nibid/config/config.toml
- sed -i 's/timeout_commit =.*/timeout_commit = "1s"/g' $HOME/.nibid/config/config.toml
- sed -i 's/skip_timeout_commit =.*/skip_timeout_commit = false/g' $HOME/.nibid/config/config.toml
+CONFIG_TOML="$HOME/.nibid/config/config.toml"
+sed -i 's/timeout_propose =.*/timeout_propose = "100ms"/g' $CONFIG_TOML
+sed -i 's/timeout_propose_delta =.*/timeout_propose_delta = "500ms"/g' $CONFIG_TOML
+sed -i 's/timeout_prevote =.*/timeout_prevote = "100ms"/g' $CONFIG_TOML
+sed -i 's/timeout_prevote_delta =.*/timeout_prevote_delta = "500ms"/g' $CONFIG_TOML
+sed -i 's/timeout_precommit =.*/timeout_precommit = "100ms"/g' $CONFIG_TOML
+sed -i 's/timeout_precommit_delta =.*/timeout_precommit_delta = "500ms"/g' $CONFIG_TOML
+sed -i 's/timeout_commit =.*/timeout_commit = "1s"/g' $CONFIG_TOML
+sed -i 's/skip_timeout_commit =.*/skip_timeout_commit = false/g' $CONFIG_TOML
+sed -i.default "s/pruning *=.*/pruning = \"custom\"/g" $HOME/.nibid/config/app.toml
+sed -i "s/pruning-keep-recent *=.*/pruning-keep-recent = \"100\"/g" $HOME/.nibid/config/app.toml
+sed -i "s/pruning-interval *=.*/pruning-interval = \"10\"/g" $HOME/.nibid/config/app.toml
  
  
- 
+pruning="custom"
+pruning_keep_recent="100"
+pruning_keep_every="0"
+pruning_interval="50"
+sed -i -e "s/^pruning *=.*/pruning = \"$pruning\"/" $HOME/.nibid/config/app.toml
+sed -i -e "s/^pruning-keep-recent *=.*/pruning-keep-recent = \"$pruning_keep_recent\"/" $HOME/.nibid/config/app.toml
+sed -i -e "s/^pruning-keep-every *=.*/pruning-keep-every = \"$pruning_keep_every\"/" $HOME/.nibid/config/app.toml
+sed -i -e "s/^pruning-interval *=.*/pruning-interval = \"$pruning_interval\"/" $HOME/.nibid/config/app.toml
 
 
 
